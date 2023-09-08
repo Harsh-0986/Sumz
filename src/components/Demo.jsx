fa0f54e03631dd2fe031f321fb91d4dc03564997
Harsh-0986 <harsh.r.shah2102@gmail.com>
2023-09-08 22:36:50 +0530

    Completed Demo section and starting backend

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {linkIcon} from "../assets";
 
 const Demo = () => {
+    const [article, setArticle] = useState({
+        url: "",
+        summary: ""
+    });
+
+    const handleSubmit = async (e) => {}
+
     return (
-        <div>Demo</div>
+        <section className="mt-16 w-full max-w-xl">
+            <div className="flex flex-col w-full gap-2">
+                {/* Search */}
+                <form onSubmit={handleSubmit} className="relative flex justify-center items-center">
+                    <img src={linkIcon} alt={"link_icon"} className={"absolute left-0 my-2 ml-3 w-5"}/>
+                    <input type={"url"} placeholder={"Enter a URL"} value={article.url} onChange={(e) => {setArticle({...article, url: e.target.value})}} required className={"url_input peer"}/>
+                    <button className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700" type={"submit"}>↵</button>
+                </form>
+                {/* Browse URL History */}
+
+            </div>
+
+            {/* Display results */}
+        </section>
     )
 }
 export default Demo

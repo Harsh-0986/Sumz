b878b8bab2170d93a8d5c001ec2636a21658a67d
Harsh-0986 <harsh.r.shah2102@gmail.com>
2023-09-08 22:19:07 +0530

    Completed Hero section

---
+++ src/components/Demo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Demo = () => {
+    return (
+        <div>Demo</div>
+    )
+}
+export default Demo
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { logo } from '../assets'
+
+const Hero = () => {
+    return (
+        <header className="w-full flex justify-center items-center flex-col">
+            <nav className="flex justify-between items-center w-full mb-10 pt-3">
+                <img src={logo} alt="Sumz Logo" className={"w-28 object-contain"}/>
+
+                <button type={"button"} onClick={() =>window.open('https://github.com/Harsh-0986/Sumz')} className={"black_btn"}>Github</button>
+            </nav>
+
+            <h1 className="head_text">Summarize articles with <br className={"max-md:hidden"}/>
+            <span className={"orange_gradient"}>OpenAI's GPT-4</span></h1>
+            <h2 className={"desc"}>
+                Simplify your reading with Sumz, an open source article summarizer that transforms lengthy articles into clear and concise summaries
+            </h2>
+        </header>
+    )
+}
+export default Hero
